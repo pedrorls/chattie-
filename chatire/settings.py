fd5ab1678c209cd38b96c804001223cac5671f21
Pedro Silva <pedro.rls93@gmail.com>
2018-01-31 16:27:59 -0200

    rest auth was added in settings

--- chatire/settings.py
+++ chatire/settings.py
@@ -129,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
